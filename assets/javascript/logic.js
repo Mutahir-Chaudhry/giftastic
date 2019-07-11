6ff338a735e0ce02f5d78f05542c9472c430ffd3
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-07-11 15:08:21 -0400

    Created a variable to create a new div every time our previously mentioned for loop iterates. Have the class newDiv added to our variable newDiv.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // created an array for all of our initial buttons. Going with the animal theme. 
     var buttonsShown = ["Lion", "Cat", "Dog", "Zebra", "Monkey", "Horse"];
     // began the function to actually pull and populate the displaygif with the requested api call.
-    function displayGIFs(){
+    function displayGIFs (){
         //Need to first empty the gif display so we dont have it putting new gifs on previous gifs.
         $("#displaygifs").empty();
         //Created all necessary initial varaibles.
@@ -16,6 +16,12 @@ $(document).ready(function(){
         }).done(function(animals){
             //Creating a for loop to create a new div everytime there is a call to the API, for up to the limit of gifs, which is defaulted to 10.
             for(var i = 0; i < limit; i++){
+                //Creating a variable that will create new div for every iteration of the loop.
+                var newDiv = $("<div>");
+                //Need each div to have its own class
+                newDiv.addClass("newDiv");
+
+                
 
             }
         })
