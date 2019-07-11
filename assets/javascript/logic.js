8199232f86ae20a15cc376b90f6dbb3c0107451f
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-07-11 16:12:27 -0400

    Having some sort of issue with logic.js connecting to our index.html. working on that. While doing so, created the function for creating and displaing buttons based on the animal names in our buttonsShown array

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    console.log("hello")
     // created an array for all of our initial buttons. Going with the animal theme. 
     var buttonsShown = ["Lion", "Cat", "Dog", "Zebra", "Monkey", "Horse"];
     // began the function to actually pull and populate the displaygif with the requested api call.
@@ -13,7 +14,7 @@ $(document).ready(function(){
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).done(function(animals){
+        }).done(function(response){
             //Creating a for loop to create a new div everytime there is a call to the API, for up to the limit of gifs, which is defaulted to 10.
             for(var i = 0; i < limit; i++){
                 //Creating a variable that will create new div for every iteration of the loop.
@@ -23,19 +24,41 @@ $(document).ready(function(){
                 //Created a variable that will create a new img within the div created. We will then use this new varaible to pull the different attributes from the API.
                 var gif = $("<img>");
                 //Now we will reference each part of the new image called and assign different parts of the response data to each part of the image.
-                gif.attr("src", reponse.data[i].images.original_still.url);
+                gif.attr("src", response.data[i].images.original_still.url);
                 gif.attr("data-state", "still");
                 gif.attr("data-still", response.data[i].images.orignal_still.url);
                 gif.attr("class", "gif");
                 gif.attr("data-animate", response.data[i].images.orginal.url);
-                
-
-        
-              
-
-
-
+                //After assigning the respective data from the response from the API to the gif to be displayed, we now add this gif to the newDiv variable we created.
+                newDiv.append(gif)
+                //Now we need to create a variable to store the rating retrieved from the gif data. 
+                var ratings = response.data[i].rating;
+                //After creating the variable ratings to store the rating, we add this to the newDiv variable we just created, but we want it to be dispayed with a label, so we have to assign it to another varaible that will be appended to our newDiv.
+                var  ratingsDisplay = $("<p>").text("The Rating for this gif is: " + ratings)
+                newDiv.append(ratingsDisplay)
+                //Now that everything has been added to the new div created everytime we pull a gif from our API, we now add all this to our displaygifs div already within our html.
+                $("#displaygifs").append(newDiv);
             }
-        })
+        });
     }
+    //Now that we've created the function to include our AJAX call to our API to retrieve the gifs, we will make a function to populate our buttons.
+    function createButtons(){
+        //Need to first empty the button div so we can run our for loop to poplate as many buttons as there are strings in our buttonsShown array.
+        $("#buttons").empty();
+        //We create a for loop to run for the iteration of our buttonsShown array.
+        for (var i = 0; i < buttonsShown.length; i++){
+            //Everytime our loop runs, a new button will be created and stored in our newButton vairable. 
+            var newButton = $("<button>")
+            //For every new button created, we want to assign specific attributes, inlcuding a class, id, and name, that will be pulled from the name provided. 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", displayedButtons[i]); 
+            newButton.text(displayedButtons[i]); 
+            //We will then add this new button to the our buttons div. 
+            $("#buttons").append(newButton); 
+        }
+    }
+
+
 })
+
