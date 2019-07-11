f2e22d71e012aad148d3eba343cc1a68df7717e9
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-07-11 14:54:56 -0400

    created inital variables with limit, applied the formula for the queryURL to incorporate the limit and user input when calling api. Now will set up AJAX to actually call the API.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+    // created an array for all of our initial buttons. Going with the animal theme. 
     var animalButtons = ["Lion", "Cat", "Dog", "Zebra", "Monkey", "Horse"];
-    
+    // began the function to actually pull and populate the displaygif with the requested api call.
     function displayGIFs(){
         $("#displaygifs").empty();
+
+        var userInput = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=oI7YWn3aO9m0rxfhuemjd1sPvKxsUk2D&q=" + input + "&limit=" + limit + "&offset=0&rating=G&lang=en"
+
+        
     }
 })
