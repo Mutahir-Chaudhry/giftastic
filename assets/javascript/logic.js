8a01cf581c3122a6b7926b0bd720df7b60d66f83
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-07-11 15:03:48 -0400

    Added AJAX method to call API from giphy. once done, creating a function to run a for loop to display gifs within their own div. So far, just for loop done. Need to add divs, and eventually images.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
     // created an array for all of our initial buttons. Going with the animal theme. 
-    var animalButtons = ["Lion", "Cat", "Dog", "Zebra", "Monkey", "Horse"];
+    var buttonsShown = ["Lion", "Cat", "Dog", "Zebra", "Monkey", "Horse"];
     // began the function to actually pull and populate the displaygif with the requested api call.
     function displayGIFs(){
+        //Need to first empty the gif display so we dont have it putting new gifs on previous gifs.
         $("#displaygifs").empty();
-
-        var userInput = $(this).attr("data-name");
+        //Created all necessary initial varaibles.
+        var input = $(this).attr("data-name");
         var limit = 10;
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=oI7YWn3aO9m0rxfhuemjd1sPvKxsUk2D&q=" + input + "&limit=" + limit + "&offset=0&rating=G&lang=en"
+        //AJAX used to utilize API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(animals){
+            //Creating a for loop to create a new div everytime there is a call to the API, for up to the limit of gifs, which is defaulted to 10.
+            for(var i = 0; i < limit; i++){
 
-        
+            }
+        })
     }
 })
